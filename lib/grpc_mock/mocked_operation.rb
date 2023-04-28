99d30985b249f3905dc275131a1a0b41158b6c7b
buzz <buzz@watcha.com>
2023-04-28 18:04:31 +0900

    operation attributes

--- lib/grpc_mock/mocked_operation.rb
+++ lib/grpc_mock/mocked_operation.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module GrpcMock
-  MockedOperation = Struct.new(:call, :metadata, :deadline)
+  MockedOperation = Struct.new(:call, :metadata, :deadline, :trailing_metadata, :cancelled?)
 end
